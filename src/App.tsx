023f3d1e1788d06921a20118bf997e7e7b2b3d06
nandoerni <nando.erni@outlook.de>
2025-01-17 23:22:07 +0100

    go

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Nando Erni</p>
+        <p>nando erni</p>
       </header>
     </div>
   );
